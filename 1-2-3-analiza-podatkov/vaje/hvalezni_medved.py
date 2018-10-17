1832fab92cd04788aef3252455e03b68cd16bbee
xlMuRlx <andraz.mur44@gmail.com>
2018-10-17 16:19:50 +0200

    Končane 1. vaje

---
+++ 1-2-3-analiza-podatkov/vaje/hvalezni_medved.py
@@ -0,0 +1,85 @@
+###############################################################################
+# Hvaležni medved
+#
+# Pri tej nalogi bomo napisali nekaj funkcij, ki nam bodo v pomoč pri analizi
+# literarnih besedil, kot je na primer koroška narodna pripovedka *Hvaležni
+# medved*.
+###############################################################################
+
+test_text = """Gori nekje v gorah, ne ve se več, ali je bilo pri Macigoju ali
+Naravniku, je šivala gospodinja v senci pod drevesom in zibala otroka. Naenkrat
+prilomasti - pa prej ni ničesar opazila - medved in ji moli taco, v kateri je
+tičal velik, debel trn. Žena se je prestrašila, a medved le milo in pohlevno
+godrnja. Zato se žena ojunači in mu izdere trn iz tace. Mrcina kosmata pa zvrne
+zibel, jo pobaše in oddide. Čez nekaj časa pa ji zopet prinese zibel, a zvhano
+napolnjeno s sladkimi hruškami . Postavil jo je na tla pred začudeno mater in
+odracal nazaj v goščavo. "Poglej no", se je razveselila mati, "kakšen hvaležen
+medved. Zvrhano zibelko sladkih hrušk mi je prinesel za en sam izdrt trn"."""
+
+###############################################################################
+# 1) Sestavite funkcijo [find_words], ki vrne množico vseh besed, ki se
+#    pojavijo v nizu in vsebujejo dan podniz.
+#
+# Namig: Pomagajte si z regex znakom za mejo [\b].
+#
+# >>> find_words(test_text, 'de')
+# {'izdere', 'debel', 'oddide', 'začudeno'}
+###############################################################################
+
+import re
+
+def find_words(besedilo, niz):
+    mnozica = set()
+    vzorec = r"\b" + r"\w*" + niz + r"\w*" + r"\b"
+    for ujemanje in re.findall(vzorec, besedilo):
+        mnozica.add(ujemanje)
+    return mnozica
+
+
+
+
+###############################################################################
+# 2) Sestavite funkcijo [find_prefix], ki vrne množico vseh besed, ki se
+#    pojavijo v nizu in imajo dano predpono.
+#
+# >>> find_prefix(test_text, 'zi')
+# {'zibala', 'zibel', 'zibelko'}
+###############################################################################
+
+def find_prefix(besedilo, niz):
+    mnozica = set()
+    vzorec = r"\b" + niz + r"\w*" + r"\b"
+    for ujemanje in re.findall(vzorec, besedilo):
+        mnozica.add(ujemanje)
+    return mnozica
+
+
+###############################################################################
+# 3) Sestavite funkcijo [find_suffix], ki vrne množico vseh besed, ki se
+#    pojavijo v nizu in imajo dano pripono.
+#
+# >>> find_suffix(test_text, 'la')
+# {'zibala', 'razveselila', 'prestrašila', 'šivala', 'opazila', 'tla'}
+###############################################################################
+
+def find_suffix(besedilo, niz):
+    mnozica = set()
+    vzorec = r"\b" + r"\w*" + niz + r"\b"
+    for ujemanje in re.findall(vzorec, besedilo):
+        mnozica.add(ujemanje)
+    return mnozica
+
+###############################################################################
+# 4) Sestavite funkcijo [double_letters], ki sprejme niz in vrne množico vseh
+#    besed, ki vsebujejo podvojene črke.
+#
+# >>> double_letters('A volunteer is worth twenty pressed men.')
+# {'volunteer', 'pressed'}
+###############################################################################
+
+def double_letters(besedilo):
+    mnozica = set()
+    vzorec = r"(\b\w*(\w)\2\w*\b)"
+    for ujemanje in re.findall(vzorec, besedilo):
+        mnozica.add(ujemanje[0])
+    return mnozica
---
+++ 4-uvod-v-ocaml/vaje/uvod.ml
@@ -0,0 +1,132 @@
+
+(* ========== Vaja 1: Uvod v OCaml  ========== *)
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [penultimate_element] vrne predzadnji element danega seznama. V
+ primeru prekratkega seznama vrne napako.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # penultimate_element [1; 2; 3; 4];;
+ - : int = 3
+[*----------------------------------------------------------------------------*)
+
+let rec penultimate_element = ()
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [get k list] poišče [k]-ti element v seznamu [list]. Številčenje
+ elementov seznama (kot ponavadi) pričnemo z 0. Če je k negativen, funkcija
+ vrne ničti element. V primeru prekratkega seznama funkcija vrne napako.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # get 2 [0; 0; 1; 0; 0; 0];;
+ - : int = 1
+[*----------------------------------------------------------------------------*)
+
+let rec get = ()
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [double] podvoji pojavitve elementov v seznamu.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # podvoji [1; 2; 3];;
+ - : int list = [1; 1; 2; 2; 3; 3]
+[*----------------------------------------------------------------------------*)
+
+let rec double = ()
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [divide k list] seznam razdeli na dva seznama. Prvi vsebuje prvih [k]
+ elementov, drugi pa vse ostale. Funkcija vrne par teh seznamov. V primeru, ko
+ je [k] izven mej seznama, je primeren od seznamov prazen.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # divide 2 [1; 2; 3; 4; 5];;
+ - : int list * int list = ([1; 2], [3; 4; 5])
+ # divide 7 [1; 2; 3; 4; 5];;
+ - : int list * int list = ([1; 2; 3; 4; 5], [])
+[*----------------------------------------------------------------------------*)
+
+let rec divide = ()
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [delete k list] iz seznama izbriše [k]-ti element. V primeru
+ prekratkega seznama funkcija vrne napako.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # delete 3 [0; 0; 0; 1; 0; 0];;
+ - : int list = [0; 0; 0; 0; 0]
+[*----------------------------------------------------------------------------*)
+
+let rec delete = ()
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [slice i k list] sestavi nov seznam, ki vsebuje elemente seznama
+ [list] od vključno [i]-tega do izključno [k]-tega. Predpostavimo, da sta [i] in
+ [k] primerna.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # slice 3 6 [0; 0; 0; 1; 2; 3; 0; 0];;
+ - : int list = [1; 2; 3]
+[*----------------------------------------------------------------------------*)
+
+let rec slice = ()
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [insert x k list] na [k]-to mesto seznama [list] vrine element [x].
+ Če je [k] izven mej seznama, ga funkcija doda na začetek oziroma na konec.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # insert 1 3 [0; 0; 0; 0; 0];;
+ - : int list = [0; 0; 0; 1; 0; 0]
+ # insert 1 (-2) [0; 0; 0; 0; 0];;
+ - : int list = [1; 0; 0; 0; 0; 0]
+[*----------------------------------------------------------------------------*)
+
+let rec insert = ()
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [rotate n list] seznam zavrti za [n] mest v levo. Predpostavimo, da
+ je [n] v mejah seznama.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # rotate 2 [1; 2; 3; 4; 5];;
+ - : int list = [3; 4; 5; 1; 2]
+[*----------------------------------------------------------------------------*)
+
+let rec rotate = ()
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [remove x list] iz seznama izbriše vse pojavitve elementa [x].
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # remove 1 [1; 1; 2; 3; 1; 2; 3; 1; 1];;
+ - : int list = [2; 3; 2; 3]
+[*----------------------------------------------------------------------------*)
+
+let rec remove = ()
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [is_palindrome] za dani seznam ugotovi ali predstavlja palindrom.
+ Namig: Pomagaj si s pomožno funkcijo, ki obrne vrstni red elementov seznama.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # is_palindrome [1; 2; 3; 2; 1];;
+ - : bool = true
+ # is_palindrome [0; 0; 1; 0];;
+ - : bool = false
+[*----------------------------------------------------------------------------*)
+
+let rec is_palindrome = ()
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [max_on_components] sprejme dva seznama in vrne nov seznam, katerega
+ elementi so večji od istoležnih elementov na danih seznamih. Skupni seznam ima
+ dolžino krajšega od danih seznamov.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # max_on_components [5; 4; 3; 2; 1] [0; 1; 2; 3; 4; 5; 6];;
+ - : int list = [5; 4; 3; 3; 4]
+[*----------------------------------------------------------------------------*)
+
+let rec max_on_components = ()
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [second_largest] vrne drugo največjo vrednost v seznamu. Pri tem se
+ ponovitve elementa štejejo kot ena vrednost. Predpostavimo, da ima seznam vsaj
+ dve različni vrednosti.
+ Namig: Pomagaj si s pomožno funkcijo, ki poišče največjo vrednost v seznamu.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # second_largest [1; 10; 11; 11; 5; 4; 10];;
+ - : int = 10
+[*----------------------------------------------------------------------------*)
+
+let rec second_largest = ()
