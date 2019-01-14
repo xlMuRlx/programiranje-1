4ccb931e08c0e3ce574eb07371ad4ed66bd0940e
xlMuRlx <andraz.mur44@gmail.com>
2019-01-14 11:55:13 +0100

    Vaje

--- 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
+++ 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
@@ -72,7 +72,7 @@ let max_cheese cheese_matrix =
   memoised_max_cheese (0, 0)
 
 
-(* Funkcija deluje hitreje s pomočjo memeoizacije, saj si vmesne rezultate zapomni in 
+(* Funkcija deluje hitreje s pomočjo memoizacije, saj si vmesne rezultate zapomni in 
 jih zato ni potrebno računati večkrat. *)
 
 
@@ -92,3 +92,34 @@ jih zato ni potrebno računati večkrat. *)
  - : int = 35
 [*----------------------------------------------------------------------------*)
 
+
+
+(*----------------------------------------------------------------------------*]
+ Na nagradni igri ste zadeli kupon, ki vam omogoča, da v Mercatorju kupite
+ poljubne izdelke, katerih skupna masa ne presega [max_w] kilogramov. Napišite
+ funkcijo [best_value articles max_w], ki poišče največjo skupno ceno, ki jo
+ lahko odnesemo iz trgovine, kjer lahko vsak izdelek vzamemo večkrat, nato pa
+ še funkcijo [best_value_uniques articles max_w], kjer lahko vsak izdelek
+ vzamemo kvečjemu enkrat.
+ Namig: Modul [Array] ponuja funkcije kot so [map], [fold_left], [copy] in
+ podobno, kot alternativa uporabi zank.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # best_value articles 1.;;
+ - : float = 10.95
+ # best_value_unique articles 1.;;
+- : float = 7.66
+[*----------------------------------------------------------------------------*)
+
+(* Articles are of form (name, price, weight) *)
+let articles = [|
+	("yoghurt", 0.39, 0.18);
+	("milk", 0.89, 1.03);
+  ("coffee", 2.19, 0.2);
+  ("butter", 1.49, 0.25);
+  ("yeast", 0.22, 0.042);
+  ("eggs", 2.39, 0.69);
+  ("sausage", 3.76, 0.50);
+  ("bread", 2.99, 1.0);
+  ("Nutella", 4.99, 0.75);
+  ("juice", 1.15, 2.0)
+|]
--- 12-dinamicno-programiranje/vaje/dinamicno_programiranje.py
+++ 12-dinamicno-programiranje/vaje/dinamicno_programiranje.py
@@ -1,3 +1,5 @@
+# 1. naloga
+
 test_matrix = [[1, 2, 0], [2, 4, 5], [7, 0, 1]]
 
 def max_cheese (cheese_matrix):
@@ -17,5 +19,61 @@ def max_cheese (cheese_matrix):
 
 
 
-def alternating_towers (visina):
-    
+
+# 3. naloga
+
+from functools import lru_cache
+
+articles = [
+    ("yoghurt", 0.39, 0.18),
+    ("milk", 0.89, 1.03),
+    ("coffee", 2.19, 0.2),
+    ("butter", 1.49, 0.25),
+    ("yeast", 0.22, 0.042),
+    ("eggs", 2.39, 0.69),
+    ("sausage", 3.76, 0.50),
+    ("bread", 2.99, 1.0),
+    ("Nutella", 4.99, 0.75),
+    ("juice", 1.15, 2.0)
+]
+
+
+def best_value(articles, max_w):
+
+    @lru_cache(maxsize=None) # memoizacija v Pythonu
+    def best_val(w):
+        options = []
+        for item in articles:
+            (name, price, weight) = item
+            if (w - weight) < 0:
+                pass
+            else:
+                option = best_val(w - weight) + price
+                options.append(option)
+        if options:
+            return max(options)
+        else:
+            return 0
+    return best_val(max_w)
+
+
+
+def best_value_unique(articles, max_w):
+    # taken je string, pri katerem taken[n] = "0" označuje da n-ti izdelek še ni bil izbran
+
+    @lru_cache(maxsize=None)
+    def best_val(w, taken):
+        options = []
+        for i, item in enumerate(articles):
+            (name, price, weight) = item
+            if (w - weight < 0) or (taken[i] == "1"):
+                pass
+            else:
+                new_taken = taken[:i] + "1" + taken[i+1:]
+                option = best_val(w - weight, new_taken) + price
+                options.append(option)
+        if options:
+            return max(options)
+        else:
+            return 0
+    return best_val(max_w, "0" * len(articles))
